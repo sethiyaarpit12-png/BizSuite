14eb4f030d9a5ed7f2aec426b906c4f281936bbf
sethiyaarpit12-png <sethiyaarpit12@gmail.com>
2026-04-14 10:15:27 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose safe APIs to the renderer
 contextBridge.exposeInMainWorld('electronAPI', {
-  onSyncFromMobile: (callback) => ipcRenderer.on('sync-from-mobile', (event, data) => callback(data)),
+  // Logo/signature saved to AppData files (Priority 4)
+  saveLogo: (firmId, type, data) => ipcRenderer.invoke('save-logo', { firmId, type, data }),
+  getLogo:  (firmId, type)       => ipcRenderer.invoke('get-logo',  { firmId, type }),
+  // Manual backup trigger
+  doBackup: (label) => ipcRenderer.invoke('do-backup', label),
+  getBackupPath: () => ipcRenderer.invoke('get-backup-path'),
+  // Invoice saved notification (triggers auto-backup)
+  invoiceSaved: (invNo) => ipcRenderer.send('invoice-saved', invNo),
+  // Platform info
   platform: process.platform,
   version: process.versions.electron
 });
